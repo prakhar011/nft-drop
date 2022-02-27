c6fe61333a1b559da25a8f4d2c98eeb6f1de5dfd
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-02-28 01:34:31 +0530

    add CheckIfWallet is connected function

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect , useState} from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana && solana.isPhantom) {
+        console.log("Wallet Found");
+       
+      } else {
+        console.log("Solana object not found, Please get a Phantom wallet");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
   return (
     <div className="App">
       <div className="container">
