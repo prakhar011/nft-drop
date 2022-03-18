417a8091c20c79c1dd7cdf13bfc34506522d6e90
prakhar011 <prakharawasthi.pa57@gmail.com>
2022-03-18 23:50:04 +0530

    get nft data and render it

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect , useState} from "react";
 import "./App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = "pprraakkhhaarrr";
@@ -58,7 +59,7 @@ const App = () => {
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
         </div>
-        {!walletAddress && renderNotConnectedContainer()}
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
