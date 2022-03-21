c3cc17197449f688db2ee45c3c9579a9ae2a1f97
Prakhar Awasthi <prakharawasthi.pa57@gmail.com>
2022-03-21 23:57:23 +0530

    edit

--- app/src/App.js
+++ app/src/App.js
@@ -67,7 +67,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
